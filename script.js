02aeb101c237c75b4ae78cf6ff56c240d44b2596
Thawane <thawaneapoliveira@gmail.com>
2022-08-02 15:14:12 -0300

    Adiciona requisito 2

---
+++ script.js
@@ -0,0 +1,15 @@
+let quadrado0 = document.getElementsByClassName('color')[0];
+quadrado0.style.backgroundColor = 'black';
+
+let quadrado1 = document.getElementsByClassName('color')[1];
+quadrado1.style.backgroundColor = 'yellowgreen';
+
+let quadrado2 = document.getElementsByClassName('color')[2];
+quadrado2.style.backgroundColor = 'salmon';
+
+let quadrado3 = document.getElementsByClassName('color')[3];
+quadrado3.style.backgroundColor = '#7231A7';
+
+
+
+
