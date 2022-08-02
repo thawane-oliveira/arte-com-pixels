cfdf2aaa45555db016025b4e9d364c4e1d4916bb
Thawane <thawaneapoliveira@gmail.com>
2022-08-02 20:24:41 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -13,43 +13,48 @@ quadrado3.style.backgroundColor = '#7231A7';
 //
 
 function criaElemento() {
-  for (let index = 0; index < 25; index += 1) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
+    for (let index = 0; index < 25; index += 1) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
 
-    const paiDoPixel = document.getElementsByTagName('section')[1];
+        const paiDoPixel = document.getElementsByTagName('section')[1];
 
-    paiDoPixel.appendChild(pixel);
-  }
+        paiDoPixel.appendChild(pixel);
+    }
 } criaElemento();
 
 //
 
 window.onload = function () {
-  const corInicial = document.querySelector('.black');
-  corInicial.classList.add('selected');
+    const corInicial = document.querySelector('.black');
+    corInicial.classList.add('selected');
 };
 
 //
 
-function removeColor () {
-  const corDescartada = document.getElementsByClassName('selected');
-  corDescartada[0].classList.remove('selected');
+const coresDisponiveis = document.querySelectorAll('.color');
+
+function selectColor(event) {
+    for (let index = 0; index < coresDisponiveis.length; index += 1) {
+        coresDisponiveis[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
 }
 
-// function selectColor () {
-//     const corEscolhida = document.getElementsByClassName('selected');
-// }
+for (let index = 0; index < coresDisponiveis.length; index += 1) {
+    coresDisponiveis[index].addEventListener('click', selectColor)
+}
 
-//
+
+// 9
 
 const clear = document.querySelector('#clear-board');
 const pixelColorido = document.querySelector('.pixel');
 
 function limpaQuadro() {
-  clear.addEventListener('click', function () {
-    if (pixelColorido.style.backgroundColor !== 'white') {
-      pixelColorido.style.backgroundColor === 'white';
-    }
-  });
+    clear.addEventListener('click', function () {
+        if (pixelColorido.style.backgroundColor !== 'white') {
+            pixelColorido.style.backgroundColor === 'white';
+        }
+    });
 } limpaQuadro();
