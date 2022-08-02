385217c3fa43c927146abc6d123e47a294cd93fb
Thawane <thawaneapoliveira@gmail.com>
2022-08-02 16:41:30 -0300

    Adiciona requisitos 4 e 5

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ quadrado2.style.backgroundColor = 'salmon';
 
 let quadrado3 = document.getElementsByClassName('color')[3];
 quadrado3.style.backgroundColor = '#7231A7';
+
+//
+
+function criaElemento() {
+
+for (let index = 0; index < 25; index += 1){
+
+let pixel = document.createElement('div');
+
+pixel.classList.add('pixel');
+
+let paiDoPixel = document.getElementsByTagName('section')[1];
+
+paiDoPixel.appendChild(pixel);
+
+}
+} criaElemento();
